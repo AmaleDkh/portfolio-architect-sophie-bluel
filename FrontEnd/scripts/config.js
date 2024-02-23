dc3ee2d17c996700403f6ea5bd9fa25dcce4c2f7
Amale Dkhissi <amdkhissi@gmail.com>
2024-02-23 21:28:32 +0100

    add projects to portfolio

---
+++ FrontEnd/scripts/config.js
@@ -0,0 +1,34 @@
+async function showProjects() {
+    const urlGetProjects = 'http://localhost:5678/api/works'
+    const listProjects = await fetch(urlGetProjects).then(response => response.json());
+
+    const portfolioProjects = document.querySelector("#portfolio");
+
+    const gallery = document.querySelector(".gallery");
+
+    while (gallery.firstChild) {
+        gallery.removeChild(gallery.firstChild);
+    }
+
+    for (let i = 0; i < listProjects.length; i++) {
+        const img = listProjects[i].imageUrl;
+        const imgProject = document.createElement('img');
+        imgProject.src = img;
+        imgProject.append(img);
+
+        const title = listProjects[i].title;
+        const titleProject = document.createElement("figcaption");
+        titleProject.append(title);
+
+        const project = document.createElement("figure");
+        project.append(imgProject);
+        project.append(titleProject);
+        project.setAttribute("id", listProjects[i].id);
+
+        gallery.append(project);
+
+        portfolioProjects.append(gallery);
+    }
+}
+
+showProjects();
