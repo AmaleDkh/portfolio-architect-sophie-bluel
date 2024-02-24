e2b56d2bd731693c06f60d3dcc02d579cb6673d0
Amale Dkhissi <amdkhissi@gmail.com>
2024-02-24 19:05:41 +0100

    feat(login): handle login form

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    handleLogin();
+})
+
+function handleLogin() {
+    const logInForm = document.querySelector(".login-form");
+
+    logInForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const email = document.querySelector('input[name="email"]').value;
+
+        const password = document.querySelector('input[name="password"]').value;
+
+        fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error("La requête a échoué avec le status " + response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                localStorage.setItem("accessToken", data.token);
+
+                window.location.href = 'index.html';
+
+            })
+            .catch(error => {
+                console.error(error);
+                alert("La combinaison utilisateur / mot de passe est incorrect. Veuillez réessayer.");
+            })
+    })
+}
