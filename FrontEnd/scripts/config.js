6dfce7968ee96a4a5279ff4a16d09ac5ece858dd
Amale Dkhissi <amdkhissi@gmail.com>
2024-02-24 11:11:30 +0100

    feat(projects): add filter buttons

--- FrontEnd/scripts/config.js
+++ FrontEnd/scripts/config.js
@@ -1,9 +1,21 @@
-async function showProjects() {
-    const urlGetProjects = 'http://localhost:5678/api/works'
-    const listProjects = await fetch(urlGetProjects).then(response => response.json());
+init();
 
+async function init() {
+    const listProjects = await fetchWorks();
+    showProjects(listProjects);
+    showFilters(listProjects);
+}
+
+async function fetchWorks() {
+    const url = 'http://localhost:5678/api/works';
+    const response = await fetch(url).then(response => response.json());
+    return response;
+}
+
+// Projects presented 
+
+async function showProjects(listProjects) {
     const portfolioProjects = document.querySelector("#portfolio");
-
     const gallery = document.querySelector(".gallery");
 
     while (gallery.firstChild) {
@@ -26,9 +38,47 @@ async function showProjects() {
         project.setAttribute("id", listProjects[i].id);
 
         gallery.append(project);
-
         portfolioProjects.append(gallery);
     }
 }
 
-showProjects();
+// Projects filtered
+
+async function showFilters(listProjects) {
+    const filters = document.createElement("div");
+    filters.setAttribute("class", "buttons");
+
+    const setCategories = new Set();
+
+    for (let i = 0; i < listProjects.length; i++) {
+        const projectCategory = listProjects[i].category;
+        setCategories.add(projectCategory.name);
+    }
+
+    const categories = Array.from(setCategories);
+
+    const filterAll = document.createElement("button");
+    filterAll.innerText = "Tous";
+    filterAll.addEventListener("click", () => {
+        showProjects(listProjects);
+    })
+
+    filters.append(filterAll);
+
+    for (let i = 0; i < categories.length; i++) {
+        const buttonCategory = document.createElement("button");
+        buttonCategory.innerText = categories[i];
+
+        buttonCategory.addEventListener("click", () => {
+            const filteredProjects = listProjects.filter(project =>
+                project.category.name === categories[i]
+            )
+            showProjects(filteredProjects);
+        })
+        filters.append(buttonCategory);
+    }
+
+    const gallery = document.querySelector(".gallery");
+    const portfolio = document.querySelector("#portfolio");
+    portfolio.insertBefore(filters, gallery);
+}
